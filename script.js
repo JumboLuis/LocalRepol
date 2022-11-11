22471c1a4a4c50f459bc1fe1c9443785f1286d13
JumboLuis <116740361+JumboLuis@users.noreply.github.com>
2022-11-11 10:35:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("BIENVENIDO A LA EVALUACIÓN DE JAVASCRIPT!!!")
+function myfunction(){
+    printText=document.getElementById("repo").innerHTML
+    alert(printText)
+    updateHeader=document.getElementById("masters").innerHTML="CABECERA ACTUALIZADA."
+  
+        
+}
